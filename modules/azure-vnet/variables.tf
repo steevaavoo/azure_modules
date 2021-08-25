396cdf8232ef491c913e481365f6f62fc49b6c68
Steve Baker <steve_m_baker@hotmail.com>
2021-08-25 20:21:40 +0100

    Changed some subnet values for testing

--- modules/azure-vnet/variables.tf
+++ modules/azure-vnet/variables.tf
@@ -22,8 +22,8 @@ variable "vnet_address_space" {
 variable "subnet" {
   type = map(string)
   default = {
-    "subnet1" = "10.0.1.0/24"
+    "subnet1" = "10.0.7.0/24"
     "subnet2" = "10.0.2.0/24"
-    "subnet3" = "10.0.3.0/24"
+    "subnet3" = "10.0.6.0/24"
   }
 }

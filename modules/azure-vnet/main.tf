7f64182319407363c58b9377f211ea4bcbf7390b
Steve Baker <steve_m_baker@hotmail.com>
2021-08-25 19:05:28 +0100

    Changed subnet creation to separate resource

--- modules/azure-vnet/main.tf
+++ modules/azure-vnet/main.tf
@@ -8,11 +8,12 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.vnet.location
   resource_group_name = azurerm_resource_group.vnet.name
   address_space       = [var.vnet_address_space]
+}
 
-  for_each = var.subnet
-  subnet {
-    name           = each.key
-    address_prefix = each.value
-  }
-
+resource "azurerm_subnet" "vnet" {
+  for_each             = var.subnet
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [each.value]
 }
